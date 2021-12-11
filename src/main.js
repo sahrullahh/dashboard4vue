b25cbc2cdaeadc63be4c17c80b14e957338186f8
Sahrullah <necomcersnet@gmail.com>
2021-12-11 20:16:10 +0700

    adding chart.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,69 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+} from 'chart.js';
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
+
+
+
+
+library.add(fas)
+Vue.component('fa', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
@@ -11,3 +74,48 @@ new Vue({
 }).$mount('#app')
 
 require('./assets/sass/main.css');
+
+var ctx = document.querySelector("#myChart").getContext("2d");
+var chart = new Chart(ctx, {
+  type: 'line', // also try bar or other graph types
+
+	// The data for our dataset
+	data: {
+		labels: ["Jun 2016", "Jul 2016", "Aug 2016", "Sep 2016", "Oct 2016", "Nov 2016", "Dec 2016", "Jan 2017", "Feb 2017", "Mar 2017", "Apr 2017", "May 2017"],
+		// Information about the dataset
+    datasets: [{
+			label: "Rainfall",
+			backgroundColor: 'lightblue',
+			borderColor: 'royalblue',
+			data: [26.4, 39.8, 66.8, 66.4, 40.6, 55.2, 77.4, 69.8, 57.8, 76, 110.8, 142.6],
+		}]
+	},
+
+	// Configuration options
+	options: {
+    layout: {
+      padding: 10,
+    },
+		legend: {
+			position: 'bottom',
+		},
+		title: {
+			display: true,
+			text: 'Precipitation in Toronto'
+		},
+		scales: {
+			yAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: 'Precipitation in mm'
+				}
+			}],
+			xAxes: [{
+				scaleLabel: {
+					display: true,
+					labelString: 'Month of the Year'
+				}
+			}]
+		}
+	}
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,9 +11,85 @@
           </div>
           <div class="wrap-head">
             <div class="wrap-button">
-              <button>Manage dashbord</button>
-              <button>Create new dashbord</button>
+              <button>
+                <fa :icon="['fas', 'cog']" />
+                <span class="text">Manage Storage</span>
+              </button>
+              <button>
+                <fa :icon="['fas', 'upload']" /><span class="text"
+                  >Upload your file</span
+                >
+              </button>
             </div>
+          </div>
+        </div>
+        <div class="wrapper-card">
+          <div class="grid-container">
+            <div class="card">
+              <div class="body-card">
+                <div class="flex-card">
+                  <div class="col">
+                    <div class="icon-circle">
+                      <fa :icon="['fas', 'file']" />
+                    </div>
+                  </div>
+                  <div class="col">
+                    <h2>57</h2>
+                    <p>Files</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="body-card">
+                <div class="flex-card">
+                  <div class="col">
+                    <div class="icon-circle">
+                      <fa :icon="['fas', 'arrow-down']" />
+                    </div>
+                  </div>
+                  <div class="col">
+                    <h2>2.0 GB</h2>
+                    <p>Used space</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="body-card">
+                <div class="flex-card">
+                  <div class="col">
+                    <div class="icon-circle">
+                      <fa :icon="['fas', 'arrow-up']" />
+                    </div>
+                  </div>
+                  <div class="col">
+                    <h2>9.0 GB</h2>
+                    <p>Free space</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="body-card">
+                <div class="flex-card">
+                  <div class="col">
+                    <div class="icon-circle">
+                      <fa :icon="['fas', 'folder']" />
+                    </div>
+                  </div>
+                  <div class="col">
+                    <h2>10 GB</h2>
+                    <p>Capacity</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="wrapper-card-graph">
+          <div class="card">
+            <canvas id="myChart" width="400" height="200"></canvas>
           </div>
         </div>
       </div>
@@ -24,10 +100,12 @@
 <script>
 // @ is an alias to /src
 import Header from "../components/Header.vue";
+
 export default {
   name: "Home",
   components: {
     Header,
   },
+  mounted() {},
 };
 </script>
